1d31c506d32967f7453aba3ff7e99106e8a46d32
Anand Raj <anandallen95@gmail.com>
2020-06-10 20:03:56 +0530

    add two numbers

---
+++ Day1/add_two_number.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1, num2, sum;
+    cout<< "Enter the num1 and num2\n";
+    cin>> num1 >> num2;
+
+    sum = num1 + num2;
+    cout<< "The sum of num1 and num2 = " << sum;
+    return 0;
+}
