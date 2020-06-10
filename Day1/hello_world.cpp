2065c7fb04aa5300eb74a8d953d2673bef4430cb
Anand Raj <anandallen95@gmail.com>
2020-06-10 19:56:25 +0530

    day 1

---
+++ Day1/hello_world.cpp
@@ -0,0 +1,6 @@
+#include<iostream> //#include is a preprocessor 
+using namespace std; //type of lib to manage the cout and cin function
+int main()//main function
+{
+    cout<<"Hello world";// cout is the print output of the program
+}
---
+++ Day1/print_no_by_user.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int number; //define variable
+    cout<< "Please Enter The Number\n"; //print on the screen
+    cin>> number;   //taking input by user
+
+    cout<< "You Entered Number is= " << number; //print on screen
+    return 0;
+}
