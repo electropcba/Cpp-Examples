8780d3f4675dc6fdb7dfac06c81ab6690d2f56c0
Anand Raj <anandallen95@gmail.com>
2020-06-10 20:32:53 +0530

    size of data types

---
+++ Day1/size_of_datatypes.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //sizeof(datatype)
+    cout<< "Size of int data type= " << sizeof(int) <<endl;
+    cout<< "Size of float data type= " << sizeof(float) <<endl;
+    cout<< "Size of double data type= " << sizeof(double) <<endl;
+    cout<< "Size of char data type= " << sizeof(char) <<endl;
+
+
+
+
+}
