b7e81ea426203b08ad9fdfad469d2c5ea30e737c
Anand Raj <anandallen95@gmail.com>
2020-08-11 11:40:57 +0530

    exp2

---
+++ Arrays/exp2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[5], i;
+
+    cout << " Size of the integer in this compiler is " << sizeof(int) << "\n";
+
+    for( i=0; i<=5; i++)
+    {
+        cout << " Address of arr[" << i << "] is " << &arr[i] << "\n";
+    }
+}
